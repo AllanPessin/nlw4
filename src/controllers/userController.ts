fae54317d4e277b29c536a6f0830ba3c00a24bbb
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-03-19 08:42:42 -0300

    surveys added in database, controller and repository, new routes to survey and show

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from 'express'
-import { getRepository, ReturningStatementNotSupportedError } from 'typeorm';
-import { User } from '../models/user';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/UserRepository';
 
 class UserController {
  async create(request: Request, response: Response){
   const {name, email} = request.body;
-  const userRepository = getRepository(User);
+  const userRepository = getCustomRepository(UserRepository);
   const userAlreadyExist = await userRepository.findOne({
     email
   });
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
+import { SurveysController } from './controllers/SurveyController'
 import { UserController } from './controllers/userController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveysController();
 
-router.post("/users", userController.create)
+router.post("/users", userController.create);
+router.post("/users", userController.show);
+router.post("/surveys", surveyController.create);
 
 export { router };
